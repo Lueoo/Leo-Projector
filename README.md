01f9dbfeb48169898ee90902dd7a114a15096340
Lueoo <144060095+Lueoo@users.noreply.github.com>
2023-09-04 20:15:17 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+## Leo-Projector by Leo
+
+
+
+
+Depedencies :
+
+# qb-input
+# qb-core
+# qb-targe
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "common"
+
+name "Leo-Projector"
+description "Leo-Projector script!"
+author "Leo-Projector"
+
+client_script "cl_main.js"
+
+server_script "sv_main.js"
